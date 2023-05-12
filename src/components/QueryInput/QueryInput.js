bd0da1b70b5de343ac783447f2d88db5c154baa9
ghostb0x <ghostb0x.github@gmail.com>
2023-05-12 02:17:42 +0100

    added logic for updating queryText state within QueryInput and WordBankProvider, including insert Generated Word  at caret location and keyboard nav accessibility

--- src/components/QueryInput/QueryInput.js
+++ src/components/QueryInput/QueryInput.js
@@ -3,8 +3,53 @@ import styles from './QueryInput.module.css';
 import { WordBankContext } from '../WordBankProvider';
 
 function QueryInput() {
-  
-  const { queryText, textAreaRef } = React.useContext(WordBankContext);
+  const {
+    queryText,
+    setQueryText,
+    textAreaRef,
+    textCursor,
+    setTextCursor,
+  } = React.useContext(WordBankContext);
+
+  const updateLoc = React.useCallback(() => {
+    let cursorLoc = textAreaRef.current.selectionEnd;
+    setTextCursor(cursorLoc);
+  }, [setTextCursor, textAreaRef]);
+
+  // on mount and after each queryText update
+  // refocuses cursor at newest textCursor location
+  React.useEffect(() => {
+    textAreaRef.current.focus();
+    textAreaRef.current.selectionEnd = textCursor;
+  }, [textAreaRef, queryText, textCursor]);
+
+  // event listener for keyboard navigation
+  // within textarea to update cursor loc
+  React.useEffect(() => {
+    function handleKeyDown(event) {
+      const navigationKeys = [
+        'ArrowDown',
+        'ArrowLeft',
+        'ArrowRight',
+        'ArrowUp',
+        'End',
+        'Home',
+        'PageDown',
+        'PageUp',
+      ];
+      if (navigationKeys.includes(event.code)) {
+        // setTimeout is necessary because of an odd
+        // lag when using navkeys and updating caret loc
+        // referenced at textAreaRef.current.selectionEnd
+        // I think it's React state lag
+        setTimeout(() => updateLoc(), 100);
+      }
+    }
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [updateLoc]);
 
   return (
     <div>
@@ -12,6 +57,11 @@ function QueryInput() {
         ref={textAreaRef}
         className={styles.inputWrapper}
         value={queryText}
+        onClick={updateLoc}
+        onChange={(event) => {
+          setQueryText(event.target.value);
+          updateLoc();
+        }}
       >
         {queryText}
       </textarea>
--- src/components/WordBankProvider/WordBankProvider.js
+++ src/components/WordBankProvider/WordBankProvider.js
@@ -3,38 +3,45 @@ import React from 'react';
 export const WordBankContext = React.createContext();
 
 function WordBankProvider({ children }) {
-  
+  // QueryInput Component state and ref
+  const textAreaRef = React.useRef();
+
+  const [textCursor, setTextCursor] = React.useState(0);
+
+  const [queryText, setQueryText] = React.useState('');
+
+  function updateQueryText(toAdd) {
+    const newWord = toAdd.word;
+
+    let newQueryText =
+      queryText.slice(0, textCursor) +
+      (newWord + ' ') +
+      queryText.slice(textCursor);
+
+    setQueryText(newQueryText);
+
+    setTextCursor(textCursor + newWord.length + 1);
+  }
+
+  // WordBank State and function
   const [wordBankItems, setWordBankItems] = React.useState([]);
 
   function addToWordBank(toAdd) {
     const newWordBankItems = [...wordBankItems];
     newWordBankItems.push(toAdd);
     setWordBankItems(newWordBankItems);
-    updateQueryText(queryText, toAdd)
+    updateQueryText(toAdd);
   }
 
-
-  const [queryText, setQueryText] = React.useState('');
-
-
-  function updateQueryText(existingText, toAdd) {
-    let newQueryText = existingText;
-    const newWord = toAdd.word
-    
-    newQueryText += (newWord + " ")
-    
-    setQueryText(newQueryText)
-  }
-
-  const textAreaRef = React.useRef();
-
-
-
-  const providerValues = { 
+  // Provider Values to pass in Context
+  const providerValues = {
     wordBankItems,
     addToWordBank,
     queryText,
-    textAreaRef
+    setQueryText,
+    textAreaRef,
+    textCursor,
+    setTextCursor,
   };
 
   return (
