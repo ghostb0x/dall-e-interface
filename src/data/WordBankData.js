464f5b404c9af32f3bbcd97cdf1b29b6d693529f
ghostb0x <ghostb0x.github@gmail.com>
2023-05-22 00:45:24 +0200

    Updated WordBank data with more examples

--- src/data/WordBankData.js
+++ src/data/WordBankData.js
@@ -1,136 +1,414 @@
 export const WordBankData = [
-    {
-        category: "animals",
-        items: [
-                    {
-                        id: "animals1",
-                        word: "hummingbird",
-                        imgSrc: "https://t3.ftcdn.net/jpg/05/30/56/10/360_F_530561002_YkNWq46zY7qOZ5p4SZfv1OHJotEAXPpU.jpg",
-                    },
-                    {
-                        id: "animals2",
-                        word: "elephant",
-                        imgSrc: "https://images.unsplash.com/photo-1597489204822-6f031f10f09d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80",
-                    }, 
-                    {
-                        id: "animal3",
-                        word: "lion",
-                        imgSrc: "https://source.unsplash.com/900x600/?lion"
-                    },
-                    {
-                        id: "animal4",
-                        word: "tiger",
-                        imgSrc: "https://source.unsplash.com/900x600/?tiger"
-                    },
-                    {
-                        id: "animal5",
-                        word: "giraffe",
-                        imgSrc: "https://source.unsplash.com/900x600/?giraffe"
-                    },
-                    {
-                        id: "animal6",
-                        word: "bear",
-                        imgSrc: "https://source.unsplash.com/900x600/?bear"
-                    },
-                    {
-                        id: "animal7",
-                        word: "panda",
-                        imgSrc: "https://source.unsplash.com/900x600/?panda"
-                    },
-                    {
-                        id: "animal8",
-                        word: "penguin",
-                        imgSrc: "https://source.unsplash.com/900x600/?penguin"
-                    },
-                    {
-                        id: "animal9",
-                        word: "dolphin",
-                        imgSrc: "https://source.unsplash.com/900x600/?dolphin"
-                    },
-                    {
-                        id: "animal10",
-                        word: "octopus",
-                        imgSrc: "https://source.unsplash.com/900x600/?octopus"
-                    },
-                    {
-                        id: "animal11",
-                        word: "butterfly",
-                        imgSrc: "https://source.unsplash.com/900x600/?butterfly"
-                    },
-                    {
-                        id: "animal12",
-                        word: "bee",
-                        imgSrc: "https://source.unsplash.com/900x600/?bee"
-                    },
-                    {
-                        id: "animal13",
-                        word: "owl",
-                        imgSrc: "https://source.unsplash.com/900x600/?owl"
-                    },
-                    {
-                        id: "animal14",
-                        word: "eagle",
-                        imgSrc: "https://source.unsplash.com/900x600/?eagle"
-                    },
-                    {
-                        id: "animal15",
-                        word: "shark",
-                        imgSrc: "https://source.unsplash.com/900x600/?shark"
-                    },
-                    {
-                        id: "animal16",
-                        word: "whale",
-                        imgSrc: "https://source.unsplash.com/900x600/?whale"
-                    },
-                    {
-                        id: "animal17",
-                        word: "jellyfish",
-                        imgSrc: "https://source.unsplash.com/900x600/?jellyfish"
-                    },
-                    {
-                        id: "animal18",
-                        word: "crocodile",
-                        imgSrc: "https://source.unsplash.com/900x600/?crocodile"
-                    },
-                    {
-                        id: "animal19",
-                        word: "snake",
-                        imgSrc: "https://source.unsplash.com/900x600/?snake"
-                    },
-
-                ]
-    },
-    {
-        category: "places",
-        items: [
-                    {
-                        id: "places1",
-                        word: "Rome",
-                        imgSrc: "https://thumbs.dreamstime.com/z/rome-italy-colosseum-coliseum-sunrise-144201572.jpg",
-                    },
-                    {
-                        id: "places2",
-                        word: "New York",
-                        imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRX3KNLt_Ex9ukL22sFX1oEzZr8CJ09QIWGnaBUqVA4&s",
-                    },
-                ]
-    },
-    {
-        category: "action verbs",
-        items: [
-                    {
-                        id: "verbs1",
-                        word: "Running",
-                        imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu9fHEScGNYqCOBHcxfGWBpmC9CuvcDyhkQqSKzRHs&s",
-                    },
-                    {
-                        id: "verbs2",
-                        word: "Drinking",
-                        imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4-lGvpNqkffO4iKnzpnABolJu2KTvEsOXMgse5YaCIg&s",
-                    },
-                ]
-    },
-    
-
-
+  {
+    category: 'animals',
+    items: [
+      {
+        id: 'animals1',
+        word: 'hummingbird',
+        imgSrc:
+          'https://t3.ftcdn.net/jpg/05/30/56/10/360_F_530561002_YkNWq46zY7qOZ5p4SZfv1OHJotEAXPpU.jpg',
+      },
+      {
+        id: 'animals2',
+        word: 'elephant',
+        imgSrc:
+          'https://images.unsplash.com/photo-1597489204822-6f031f10f09d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80',
+      },
+      {
+        id: 'animal3',
+        word: 'lion',
+        imgSrc: 'https://source.unsplash.com/900x600/?lion',
+      },
+      {
+        id: 'animal4',
+        word: 'tiger',
+        imgSrc: 'https://source.unsplash.com/900x600/?tiger',
+      },
+      {
+        id: 'animal5',
+        word: 'giraffe',
+        imgSrc: 'https://source.unsplash.com/900x600/?giraffe',
+      },
+      {
+        id: 'animal6',
+        word: 'bear',
+        imgSrc: 'https://source.unsplash.com/900x600/?bear',
+      },
+      {
+        id: 'animal7',
+        word: 'panda',
+        imgSrc: 'https://source.unsplash.com/900x600/?panda',
+      },
+      {
+        id: 'animal8',
+        word: 'penguin',
+        imgSrc: 'https://source.unsplash.com/900x600/?penguin',
+      },
+      {
+        id: 'animal9',
+        word: 'dolphin',
+        imgSrc: 'https://source.unsplash.com/900x600/?dolphin',
+      },
+      {
+        id: 'animal10',
+        word: 'octopus',
+        imgSrc: 'https://source.unsplash.com/900x600/?octopus',
+      },
+      {
+        id: 'animal11',
+        word: 'butterfly',
+        imgSrc: 'https://source.unsplash.com/900x600/?butterfly',
+      },
+      {
+        id: 'animal12',
+        word: 'bee',
+        imgSrc: 'https://source.unsplash.com/900x600/?bee',
+      },
+      {
+        id: 'animal13',
+        word: 'owl',
+        imgSrc: 'https://source.unsplash.com/900x600/?owl',
+      },
+      {
+        id: 'animal14',
+        word: 'eagle',
+        imgSrc: 'https://source.unsplash.com/900x600/?eagle',
+      },
+      {
+        id: 'animal15',
+        word: 'shark',
+        imgSrc: 'https://source.unsplash.com/900x600/?shark',
+      },
+      {
+        id: 'animal16',
+        word: 'whale',
+        imgSrc: 'https://source.unsplash.com/900x600/?whale',
+      },
+      {
+        id: 'animal17',
+        word: 'jellyfish',
+        imgSrc: 'https://source.unsplash.com/900x600/?jellyfish',
+      },
+      {
+        id: 'animal18',
+        word: 'crocodile',
+        imgSrc: 'https://source.unsplash.com/900x600/?crocodile',
+      },
+      {
+        id: 'animal19',
+        word: 'snake',
+        imgSrc: 'https://source.unsplash.com/900x600/?snake',
+      },
+    ],
+  },
+  {
+    category: 'places',
+    items: [
+      {
+        id: 'country1',
+        word: 'Kenya',
+        imgSrc: 'https://source.unsplash.com/900x600/?Kenya',
+      },
+      {
+        id: 'country2',
+        word: 'Japan',
+        imgSrc: 'https://source.unsplash.com/900x600/?Japan',
+      },
+      {
+        id: 'country3',
+        word: 'Egypt',
+        imgSrc: 'https://source.unsplash.com/900x600/?Egypt',
+      },
+      {
+        id: 'country4',
+        word: 'Peru',
+        imgSrc: 'https://source.unsplash.com/900x600/?Peru',
+      },
+      {
+        id: 'country5',
+        word: 'Germany',
+        imgSrc: 'https://source.unsplash.com/900x600/?Germany',
+      },
+      {
+        id: 'country6',
+        word: 'Nigeria',
+        imgSrc: 'https://source.unsplash.com/900x600/?Nigeria',
+      },
+      {
+        id: 'country7',
+        word: 'China',
+        imgSrc: 'https://source.unsplash.com/900x600/?China',
+      },
+      {
+        id: 'country8',
+        word: 'Colombia',
+        imgSrc: 'https://source.unsplash.com/900x600/?Colombia',
+      },
+      {
+        id: 'country9',
+        word: 'Morocco',
+        imgSrc: 'https://source.unsplash.com/900x600/?Morocco',
+      },
+      {
+        id: 'country10',
+        word: 'Spain',
+        imgSrc: 'https://source.unsplash.com/900x600/?Spain',
+      },
+      {
+        id: 'country11',
+        word: 'Tanzania',
+        imgSrc: 'https://source.unsplash.com/900x600/?Tanzania',
+      },
+      {
+        id: 'country12',
+        word: 'India',
+        imgSrc: 'https://source.unsplash.com/900x600/?India',
+      },
+      {
+        id: 'country13',
+        word: 'United Arab Emirates',
+        imgSrc: 'https://source.unsplash.com/900x600/?UAE',
+      },
+      {
+        id: 'country14',
+        word: 'Brazil',
+        imgSrc: 'https://source.unsplash.com/900x600/?Brazil',
+      },
+      {
+        id: 'country15',
+        word: 'South Korea',
+        imgSrc: 'https://source.unsplash.com/900x600/?SouthKorea',
+      },
+      {
+        id: 'country16',
+        word: 'Ghana',
+        imgSrc: 'https://source.unsplash.com/900x600/?Ghana',
+      },
+      {
+        id: 'country17',
+        word: 'Indonesia',
+        imgSrc: 'https://source.unsplash.com/900x600/?Indonesia',
+      },
+      {
+        id: 'country18',
+        word: 'Mexico',
+        imgSrc: 'https://source.unsplash.com/900x600/?Mexico',
+      },
+      {
+        id: 'country19',
+        word: 'Italy',
+        imgSrc: 'https://source.unsplash.com/900x600/?Italy',
+      },
+      {
+        id: 'country20',
+        word: 'Argentina',
+        imgSrc: 'https://source.unsplash.com/900x600/?Argentina',
+      },
+      {
+        id: 'country21',
+        word: 'Thailand',
+        imgSrc: 'https://source.unsplash.com/900x600/?Thailand',
+      },
+      {
+        id: 'country22',
+        word: 'Senegal',
+        imgSrc: 'https://source.unsplash.com/900x600/?Senegal',
+      },
+      {
+        id: 'country23',
+        word: 'Turkey',
+        imgSrc: 'https://source.unsplash.com/900x600/?Turkey',
+      },
+      {
+        id: 'country24',
+        word: 'Chile',
+        imgSrc: 'https://source.unsplash.com/900x600/?Chile',
+      },
+      {
+        id: 'country25',
+        word: 'Sweden',
+        imgSrc: 'https://source.unsplash.com/900x600/?Sweden',
+      },
+      {
+        id: 'country26',
+        word: 'Saudi Arabia',
+        imgSrc: 'https://source.unsplash.com/900x600/?SaudiArabia',
+      },
+      {
+        id: 'country27',
+        word: 'Portugal',
+        imgSrc: 'https://source.unsplash.com/900x600/?Portugal',
+      },
+      {
+        id: 'country28',
+        word: 'Panama',
+        imgSrc: 'https://source.unsplash.com/900x600/?Panama',
+      },
+      {
+        id: 'country29',
+        word: 'Ukraine',
+        imgSrc: 'https://source.unsplash.com/900x600/?Ukraine',
+      },
+      {
+        id: 'country30',
+        word: 'Greece',
+        imgSrc: 'https://source.unsplash.com/900x600/?Greece',
+      },
+    ],
+  },
+  {
+    category: 'action verbs',
+    items: [
+        {
+            id: "verb1",
+            word: "Running",
+            imgSrc: "https://source.unsplash.com/900x600/?Running",
+        },
+        {
+            id: "verb2",
+            word: "Drinking",
+            imgSrc: "https://source.unsplash.com/900x600/?Drinking",
+        },
+        {
+            id: "verb3",
+            word: "Jumping",
+            imgSrc: "https://source.unsplash.com/900x600/?Jumping",
+        },
+        {
+            id: "verb4",
+            word: "Swimming",
+            imgSrc: "https://source.unsplash.com/900x600/?Swimming",
+        },
+        {
+            id: "verb5",
+            word: "Eating",
+            imgSrc: "https://source.unsplash.com/900x600/?Eating",
+        },
+        {
+            id: "verb6",
+            word: "Sleeping",
+            imgSrc: "https://source.unsplash.com/900x600/?Sleeping",
+        },
+        {
+            id: "verb7",
+            word: "Dancing",
+            imgSrc: "https://source.unsplash.com/900x600/?Dancing",
+        },
+        {
+            id: "verb8",
+            word: "Singing",
+            imgSrc: "https://source.unsplash.com/900x600/?Singing",
+        },
+        {
+            id: "verb9",
+            word: "Laughing",
+            imgSrc: "https://source.unsplash.com/900x600/?Laughing",
+        },
+        {
+            id: "verb10",
+            word: "Cycling",
+            imgSrc: "https://source.unsplash.com/900x600/?Cycling",
+        },
+        {
+            id: "verb11",
+            word: "Writing",
+            imgSrc: "https://source.unsplash.com/900x600/?Writing",
+        },
+        {
+            id: "verb12",
+            word: "Reading",
+            imgSrc: "https://source.unsplash.com/900x600/?Reading",
+        },
+        {
+            id: "verb13",
+            word: "Cooking",
+            imgSrc: "https://source.unsplash.com/900x600/?Cooking",
+        },
+        {
+            id: "verb14",
+            word: "Playing",
+            imgSrc: "https://source.unsplash.com/900x600/?Playing",
+        },
+        {
+            id: "verb15",
+            word: "Painting",
+            imgSrc: "https://source.unsplash.com/900x600/?Painting",
+        },
+        {
+            id: "verb16",
+            word: "Studying",
+            imgSrc: "https://source.unsplash.com/900x600/?Studying",
+        },
+        {
+            id: "verb17",
+            word: "Gardening",
+            imgSrc: "https://source.unsplash.com/900x600/?Gardening",
+        },
+        {
+            id: "verb18",
+            word: "Hiking",
+            imgSrc: "https://source.unsplash.com/900x600/?Hiking",
+        },
+        {
+            id: "verb19",
+            word: "Drawing",
+            imgSrc: "https://source.unsplash.com/900x600/?Drawing",
+        },
+        {
+            id: "verb20",
+            word: "Skiing",
+            imgSrc: "https://source.unsplash.com/900x600/?Skiing",
+        },
+        {
+            id: "verb21",
+            word: "Walking",
+            imgSrc: "https://source.unsplash.com/900x600/?Walking",
+        },
+        {
+            id: "verb22",
+            word: "Coding",
+            imgSrc: "https://source.unsplash.com/900x600/?Coding",
+        },
+        {
+            id: "verb23",
+            word: "Listening",
+            imgSrc: "https://source.unsplash.com/900x600/?Listening",
+        },
+        {
+            id: "verb24",
+            word: "Surfing",
+            imgSrc: "https://source.unsplash.com/900x600/?Surfing",
+        },
+        {
+            id: "verb25",
+            word: "Sculpting",
+            imgSrc: "https://source.unsplash.com/900x600/?Sculpting",
+        },
+        {
+            id: "verb26",
+            word: "Exploring",
+            imgSrc: "https://source.unsplash.com/900x600/?Exploring",
+        },
+        {
+            id: "verb27",
+            word: "Meditating",
+            imgSrc: "https://source.unsplash.com/900x600/?Meditating",
+        },
+        {
+            id: "verb28",
+            word: "Traveling",
+            imgSrc: "https://source.unsplash.com/900x600/?Traveling",
+        },
+        {
+            id: "verb29",
+            word: "Smiling",
+            imgSrc: "https://source.unsplash.com/900x600/?Smiling",
+        },
+        {
+            id: "verb30",
+            word: "Skydiving",
+            imgSrc: "https://source.unsplash.com/900x600/?Skydiving",
+        }
+    ]
+  },
 ];
